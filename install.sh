a38e4a4766997dc529dc2a0e5b77f1ea1708fa4a
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2020-05-13 19:58:18 +0200

    ...

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+echo "copying .pythonrc"
 cp .pythonrc ~
+echo "installing .bash_functions"
+cat .bash_functions > ~/.bash_functions
+echo "appending .aliases"
+cat .aliases >> ~/.aliases
+
